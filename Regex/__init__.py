b31f884cb741c0689749d8d1fcbac30bfeee667d
youngxhui <youngxhui@gmail.com>
2016-11-18 15:29:02 +0800

    字符匹配的学习

--- Regex/__init__.py
+++ Regex/__init__.py
@@ -18,3 +18,45 @@ b = re.search(bt, 'bbtbet').group()
 print('b的多字符bet的search结果是', b)
 
 # 匹配任何单字符
+'''
+单字符无法匹配\n
+'''
+
+anyend = '.end'
+
+end = re.match(anyend, 'bend').group()
+print('anyend的bend的match结果是', end)
+
+end = re.match(anyend, 'end')
+if end is not None:
+    print('end的match的结果是', end.group())
+else:
+    print('end匹配失败')
+
+end = re.match(anyend, '\nend')
+if end is not None:
+    print('end的match的结果是', end.group())
+else:
+    print('end匹配失败')
+
+end = re.search(anyend, 'the game is end')
+if end is not None:
+    print('end的search的结果是', end.group())
+else:
+    print('end匹配失败')
+
+# 创建字符集 []
+
+'''
+比较[]与|的差异
+[cr][23][dp][o2]和r2d2|c3po
+'''
+
+str1 = re.match('[cr][23][dp][o2]', 'c2do').group()
+print('str的match c2do结果为', str1)
+str2 = re.match('r2d2|c3po', 'c2do')
+if str2 is not None:
+    print('str2的match结果为', str2)
+else:
+    print('匹配失败')
+
