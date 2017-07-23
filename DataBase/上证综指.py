dc9d8784673b9eb66aa6517443cd964d0b4bbcd5
youngxhui <youngxhui@gmail.com>
2017-07-23 16:09:29 +0800

    散点图

---
+++ DataBase/上证综指.py
@@ -0,0 +1,11 @@
+# coding=utf8
+import matplotlib.pyplot as plt
+import numpy as np
+
+open,close=np.loadtxt('G:\\PythonCode\\DataBase\\000001.csv',delimiter=',',skiprows=1,usecols=(1,4),unpack=True)
+change=close-open
+print(change)
+yesterday=change[:-1]
+today=change[1:]
+plt.scatter(yesterday,today,10,'r','<',alpha=0.1)
+plt.show()
